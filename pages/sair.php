9aa64722aedbe94b48c03e4b63ad8df80a2a8640
Davi-Martins-Sousa <74196401+Davi-Martins-Sousa@users.noreply.github.com>
2024-07-17 15:16:17 -0300

    Create sair.php

---
+++ pages/sair.php
@@ -0,0 +1,23 @@
+<?php include("Cabecalho.php");?>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['sair'])) {
+    $_SESSION = array();
+
+    session_destroy();
+
+    header("Location: login.php");
+    exit();
+}
+?>
+
+<div class="container">
+    <h2>Você deseja sair?</h2>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+        <div class="col-12">
+            <input class="btn btn-primary" type="submit" value="Sair" name="sair"/>
+        </div>
+    </form>
+</div>
+
+<?php include("Rodape.php");?>
